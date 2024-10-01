86e018388c28d56ed850cb412f1b63d1b0b5fa9d
Guilherme Mendes <guilheu2004@gmail.com>
2024-10-01 15:50:37 -0300

    Adicionar arquivos de projeto.

---
+++ temAestagio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temAestagio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a palavra");
+            string palavra = Console.ReadLine();
+            int temA = 0;
+            for(int ii = 0; ii < palavra.Length; ii++)
+            {
+               temA += ((palavra[ii] == 'a') || (palavra[ii] == 'A'))?1: 0;
+            }
+            if (temA == 0)
+            {
+                Console.WriteLine($"{palavra} nao contem a");
+            }
+            else
+            {
+                Console.WriteLine($"A palavra possui {temA} letra(s) a");
+            }
+            Console.ReadKey();
+        }
+    }
+}
